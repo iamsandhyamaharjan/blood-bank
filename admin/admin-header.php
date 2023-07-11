378fc69061aa3e9653411a3571835bf0d165d5df
Sagan Shrestha <saganshrestha01@gmail.com>
2023-07-11 22:46:53 +0545

    admin-header update

---
+++ admin/admin-header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="admin-header.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="https://assets.rumsan.com/esatya/hlb-navbar-logo.png" alt="">
+        </div>
+        <div class="title">
+            <h1>Blood Bank Management System</h1>
+        </div>
+        <div class="logout">
+            <a href="../logout/logout.php">Logout</a> 
+        </div>
+    </header>
+</body>
+</html>
