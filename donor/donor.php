6713720edc70a9ab795c808b5f7aec99b4676445
Sagan Shrestha <saganshrestha01@gmail.com>
2023-07-12 22:19:24 +0545

    donor updated

--- donor/donor.php
+++ donor/donor.php
@@ -1,25 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Document</title>
-          <link rel="stylesheet" type="text/css" href="../header/header.css">
-          <link rel="stylesheet" type="text/css" href="content.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="../header/header.css">
+  <link rel="stylesheet" type="text/css" href="content.css">
   <script src="../header/header.js"></script>
-  <link rel="stylesheet" type="text/css" href="../footer/footer.css">
   <script src="../footer/footer.js"></script>
+  <link rel="stylesheet" href="../footer/footer.css">
+  <title>Document</title>
 </head>
 
-<?php
+  <?php
   // Include file2.php
-  include '../header/header.php';
+    include '../header/header.php';
   ?>
+
   Welcome donor
+
  <?php
   // Include file2.php
-  include '../footer copy/footer.php';
+    include '../footer/footer.php';
   ?> 
 </body>
 </html>
